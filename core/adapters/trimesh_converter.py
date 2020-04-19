d2511491385d790a166683b19f082d361fed93e2
carlo <cawinkelhake@aol.com>
2020-04-19 23:56:58 +0100

    random colour

--- core/adapters/trimesh_converter.py
+++ core/adapters/trimesh_converter.py
@@ -1,5 +1,6 @@
 import logging
 import trimesh
+import numpy as np
 
 
 def convert_meshes_trimesh(meshes):
@@ -8,9 +9,16 @@ def convert_meshes_trimesh(meshes):
     index = 0
     for mesh in meshes:
         mesh2 = trimesh.Trimesh(
-            vertices=mesh[0], faces=mesh[1], vertex_normals=mesh[2],
+            vertices=mesh[0],
+            faces=mesh[1],
+            vertex_normals=mesh[2],
+            vertex_colors=get_random_rgb_colours()
+            + [0.5],  # set alpha to 0.5 for all colours
         )
-
         trimesh_objects.append(mesh2)
         index += 1
     return trimesh_objects
+
+
+def get_random_rgb_colours():
+    return list(np.random.rand(3))
