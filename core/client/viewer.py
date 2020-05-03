6b1efc5d0aba518b8f1575afec7076ef8fbf4bc2
IBaskaran <zcabiba@ucl.ac.uk>
2020-05-03 13:33:45 +0100

    Final Image

--- core/client/viewer.py
+++ core/client/viewer.py
@@ -33,7 +33,7 @@ def view_mesh(meshes: list, output_file: str, mesh_names: list = [], patient_dat
 
     vp = Plotter(sharecam=False,
                  bg="./core/client/images/hologram_icon2.png",
-                 bg2='black',shape=[1,1],interactive=False)
+                 bg2='black',shape=[1,1],size=[640,480],interactive=False)
     # pos = position corner number: horizontal [1-4] or vertical [11-14]
     vp.addSlider2D(slider1, -9, 9, value=0, pos=4, title="color number")
 
@@ -81,7 +81,9 @@ def view_mesh(meshes: list, output_file: str, mesh_names: list = [], patient_dat
     for i in range(0, len(meshes)):
         vmeshes.append(trimesh2vtk(meshes[i], alphaPerCell=True))
     doc = Text2D(patient_data, pos=4,c=(0,113,197))
-    vp.backgroundRenderer.GetActiveCamera().Zoom(1.3)
     vp.show(doc)
+    vp.backgroundRenderer.GetActiveCamera().Zoom(1.3)
+
     vp.show(vmeshes)
+
     interactive()
